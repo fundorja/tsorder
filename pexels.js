b4fcda7132202e47cc09a0ba4c8bb7d209984121
Thomas <45685865+fundorja@users.noreply.github.com>
2024-09-04 19:32:00 +0200

    rng endpoint

--- pexels.js
+++ pexels.js
@@ -2,11 +2,20 @@ const apiKey = "563492ad6f917000010000018eab46c0ee374b6eb502a86a04ce109c";
 
 // const endpoont = "https://api.pexels.com/v1/curated?per_page=1";
 // const videoEndpoint = "https://api.pexels.com/videos/videos/";
-const natureEndpoint = "https://api.pexels.com/v1/search?query=breathtaking?per_page=1";
+// const natureEndpoint = "https://api.pexels.com/v1/search?query=breathtaking?per_page=1";
+const query = "breathtaking";
+const totalPages = 768;
+
+function getRandomPage() {
+    return Math.floor(Math.random() * totalPages) + 1;
+}
 
 async function setBackground() {
+    const page = getRandomPage();
+    const rngEndpoint = `https://api.pexels.com/v1/search?query=${query}&per_page=1&page=${page}`;
+
     try {
-        const response = await fetch(natureEndpoint, {
+        const response = await fetch(rngEndpoint, {
             headers: {
                 Authorization: apiKey,
             },
