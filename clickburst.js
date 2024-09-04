03a4c7e48b2a280a58c7e8fd87c7f3efc1023371
Thomas <45685865+fundorja@users.noreply.github.com>
2024-09-04 21:42:15 +0200

    mojs

--- clickburst.js
+++ clickburst.js
@@ -1,26 +1,23 @@
-if (typeof mojs === "undefined") {
-    console.error("mo.js is not loaded");
-} else {
-    const burst = new mojs.Burst({
-        left: 0,
-        top: 0,
-        radius: { 4: 19 },
-        angle: 45,
-        children: {
-            shape: "line",
-            radius: 3,
-            scale: 1,
-            stroke: "#FD7932",
-            strokeDasharray: "100%",
-            strokeDashoffset: { "-100%": "100%" },
-            duration: 700,
-            easing: "quad.out",
-        },
-    });
+import mojs from "@mojs/core";
 
-    document.addEventListener("click", function (e) {
-        burst.tune({ x: e.pageX, y: e.pageY }).replay();
-    });
+const burst = new mojs.Burst({
+    left: 0,
+    top: 0,
+    radius: { 4: 19 },
+    angle: 45,
+    children: {
+        shape: "line",
+        radius: 3,
+        scale: 1,
+        stroke: "#FD7932",
+        strokeDasharray: "100%",
+        strokeDashoffset: { "-100%": "100%" },
+        duration: 700,
+        easing: "quad.out",
+    },
+});
 
-    // new MojsPlayer({ add: burst, isPlaying: true, isRepeat: true });
-}
+document.addEventListener("click", function (e) {
+    burst.tune({ x: e.pageX, y: e.pageY }).replay();
+});
+// new MojsPlayer({ add: burst, isPlaying: true, isRepeat: true });
