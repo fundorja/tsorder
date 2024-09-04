33fe2dbd4aaee14d5eb77334e61ba280f02aae60
Thomas <45685865+fundorja@users.noreply.github.com>
2024-09-04 18:29:26 +0200

    tilt updates

--- vanilla-tilt.js
+++ vanilla-tilt.js
@@ -494,3 +494,17 @@ var VanillaTilt = (function () {
 
     return VanillaTilt;
 })();
+
+VanillaTilt.init(document.querySelector(".ts-wrap"), {
+    scale: 1.02,
+    max: 5,
+    speed: 700,
+    glare: false,
+});
+
+VanillaTilt.init(document.querySelectorAll("li"), {
+    scale: 1.05,
+    max: 5,
+    speed: 300,
+    glare: false,
+});
